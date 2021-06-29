43eb69b4e79c0203a2ce08cc0138abef6e5bd2d5
Ravish <ravishjain786@gmail.com>
2021-06-29 23:35:48 +0530

    Second Commit

--- app/src/main/java/com/ravish/quicknews/constants/NewsUrls.java
+++ app/src/main/java/com/ravish/quicknews/constants/NewsUrls.java
@@ -3,6 +3,8 @@ package com.ravish.quicknews.constants;
 public class NewsUrls {
 
     public static final String BASE_URL = "https://newsapi.org/v2/";
+
+    // Enter API Key Here
     public static final String API_KEY = "YOUR_API_KEY";
 
     public static final String ENDPOINT_NEWS_URL = "top-headlines?country=in&category=business&apiKey=" + API_KEY;
